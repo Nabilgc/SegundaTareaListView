13ab3ebc1aa345d39aa84a1a0f390ee425b29b19
Nabil Galeb <nabilgaleb@hotmail.com>
2019-06-28 18:41:30 -0400

    Folders MVVM y clase

---
+++ app/app/app/Models/Notes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace app.Models
+{
+    public class Notes
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+    }
+}
